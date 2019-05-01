b14aea316f8fff46b81b6bb3a81511f92c2828a0
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-01 19:19:57 +0500

    automatic commit and push

--- c_program.c
+++ c_program.c
@@ -1,11 +1,14 @@
 
 #include <stdio.h>
 int main(void){
-    printf("Parkinson's Law:
-Work expands so as to ");
-    printf("fill the time
-");
-    printf("available for its completion
-");
+    int height = 8;
+    int length = 12;
+    int width = 10;
+    int volume = height * length * width;
+    int weight = (volume + 165) / 166;
+    printf("Dimensions: %dx%dx%d", length, width, height);
+    printf("Volume (cubic inches): %d", volume);
+    printf("Dimensional weight (pounds): %d", weight);
+    
     return 0;
 }
