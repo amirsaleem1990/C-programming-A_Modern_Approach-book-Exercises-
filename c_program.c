a7ce9e0a5580f859c0b8f7e93d3ad0f1b2cfd250
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-01 19:50:26 +0500

    automatic commit and push

--- c_program.c
+++ c_program.c
@@ -2,6 +2,11 @@
 #include <stdio.h>
 int main(void){
     char x='*';
-    printf("%c", x);
+    printf("       %c\n", x);
+    printf("      %c\n", x);
+    printf("     %c\n", x);
+    printf("%c   %c\n", x, x);
+    printf(" %c %c\n", x, x);
+    printf("  %c \n", x);
     return 0;
 }
