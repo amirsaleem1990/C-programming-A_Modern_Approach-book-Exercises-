445c17efc5b5241bb03cc94d5d931282c5b3a621
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-01 19:09:49 +0500

    automatic commit and push

--- c_program.c
+++ c_program.c
@@ -1,5 +1,11 @@
 
 #include <stdio.h>
 int main(void){
-    printf("hello, world");
+    printf("Parkinson's Law:
+Work expands so as to ");
+    printf("fill the time
+");
+    printf("available for its completion
+");
+    return 0;
 }
