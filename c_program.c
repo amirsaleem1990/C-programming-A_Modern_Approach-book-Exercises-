75a29941f34081808d7cc0eb8748a40b07b59f71
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-01 19:40:18 +0500

    automatic commit and push

--- c_program.c
+++ c_program.c
@@ -1,9 +1,7 @@
 
 #include <stdio.h>
 int main(void){
-    int int_1, int_2, int_3;
-    float f_1, f_2, f_3;
-    printf("%d      %d      %d\n", int_1, int_2, int_3);
-    printf("%f      %f      %f\n", f_1, f_2, f_3);
+    char x='*';
+    printf("%c", x);
     return 0;
 }
