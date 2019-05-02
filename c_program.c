2e209ad738bd50bb4e651d70f4750bd430b9a66c
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-02 12:32:48 +0500

    automatic commit and push

--- c_program.c
+++ c_program.c
@@ -1,11 +1,24 @@
 
 #include <stdio.h>
 int main(void){
-    int radius;
-    printf("Enter radius (in integer form)\n");
-    scanf("%d", &radius);
-    float pie=3.14;
-    float volume = (4.0f/3.0f)*pie*(radius*radius*radius);
-    printf("Volume: %f\n", volume);
+    int dollers, twentys,balance, ten, five, one;
+    printf("Enter a number:       ");
+    scanf("%d", &dollers);
+    twentys = (dollers - (dollers % 20)) / 20;
+    balance = dollers - (twentys * 20);
+    ten = (balance - (balance % 10)) / 10;
+    balance = balance - (ten * 10);
+    five = (balance - (balance % 5)) / 5;
+    balance = balance - (five * 5);
+    one = (balance - (balance % 1)) / 1;
+    balance = balance - (one * 1);
+    printf("$20 Bills:     %d
+", twentys);
+    printf("$10 Bills:     %d
+", ten);
+    printf("$5 Bills:     %d
+", five);
+    printf("$1 Bills:     %d
+", one);
     return 0;
 }
