1636dfc38fcd71f3565664fa5e077fa7b1fa18b3
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-02 11:51:50 +0500

    automatic commit and push

--- temp.c
+++ temp.c
@@ -3,8 +3,8 @@ int main(void){
 	int dollers, twentys;
 	printf("Enter a number\n");
 	scanf("%d", &dollers);
-	twentys = (dollers - (dollers % 20)) / 20;
+	// twentys = (dollers - (dollers % 20)) / 20;
 	printf("%d", dollers % 20);
-	printf("%d", twentys);
+	// printf("%d", twentys);
     return 0;
 }
