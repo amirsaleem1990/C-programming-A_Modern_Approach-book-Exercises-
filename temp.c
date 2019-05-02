28db5196bfe3a9ba2340e202102399f645218333
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-02 12:51:10 +0500

    automatic commit and push

--- temp.c
+++ temp.c
@@ -2,12 +2,12 @@
 int main(void){
 	int loan, interest_rate, monthly_payment;
 	printf("Enter amount of loan:\n");
-	scanf("%d", &loan);
+	// scanf("%d", &loan);
 	printf("Enter interest rate:\n");
 	scanf("%d", &interest_rate);
-	interest_rate = (interest_rate/100)/12
+	interest_rate = (interest_rate/100)/12;
 	printf("Enter monthly payment:\n");
-	scanf("%d", &monthly_payment);
-	printf("%d", interest_rate)
+	// scanf("%d", &monthly_payment);
+	printf("%d", interest_rate);
     return 0;
 }
