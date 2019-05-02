e430798d92516448a45132a7606436b365d0d4f2
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-02 13:01:40 +0500

    automatic commit and push

--- temp.c
+++ temp.c
@@ -8,9 +8,9 @@ int main(void){
 	interest_rate = interest_rate/100/12;
 	printf("Enter monthly payment:\n");
 	scanf("%f", &monthly_payment);
-	f_m = loan - (loan * interest_rate) - monthly_payment;
-	s_m = f_m - (f_m * interest_rate) - monthly_payment;
-	t_m = s_m - (s_m * interest_rate) - monthly_payment;
+	f_m = loan + (loan * interest_rate) - monthly_payment;
+	s_m = f_m + (f_m * interest_rate) - monthly_payment;
+	t_m = s_m + (s_m * interest_rate) - monthly_payment;
 	printf("Balance remaining after first payment: $%f\n", f_m);
 	printf("Balance remaining after second payment: $%f\n", s_m);
 	printf("Balance remaining after third payment: $%f\n", t_m);
