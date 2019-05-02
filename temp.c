ea3ec4c4c6cdaf19335cdb30c897d1ee7eba511f
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-02 11:22:08 +0500

    automatic commit and push

--- temp.c
+++ temp.c
@@ -3,6 +3,6 @@ int main(void){
     // int radius=10, pie=3.14;
     // float volume = (4.0f/3.0f)*pie*(radius*radius*radius);
     // printf("Volume: %f\\n", volume);
-    printf("%f", (4.0f/3.0f)*3.14);
+    printf("%f", (4.0f/3.0f)*3.14*(10*10*10));
     return 0;
 }
