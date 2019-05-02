bc4b7b8a979e159c60d39d343623dbeb8c68eddb
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-02 12:17:55 +0500

    automatic commit and push

--- temp.c
+++ temp.c
@@ -9,10 +9,11 @@ int main(void){
 	balance = balance - (balance * 10);
 	if ((balance % 5) > 5){
 		five = (balance - (balance % 5)) / 5;
-		};
-	ifelse{
+		else {
 		five = 0;
 	};
+		};
+	
 	balance = balance - (balance * 5);
 	one = (balance - (balance % 1)) / 1;
 	balance = balance - (balance * 1);
