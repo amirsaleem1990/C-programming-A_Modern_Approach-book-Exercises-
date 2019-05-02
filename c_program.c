dc44248e2452ec9c4f1bd41e4634cc6be1eb5a1e
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-02 11:26:00 +0500

    automatic commit and push

--- c_program.c
+++ c_program.c
@@ -1,7 +1,9 @@
 
 #include <stdio.h>
 int main(void){
-    int radius=10;
+    int radius;
+    printf("Enter radius (in integer form)\n");
+    scanf("%d", &radius);
     float pie=3.14;
     float volume = (4.0f/3.0f)*pie*(radius*radius*radius);
     printf("Volume: %f\n", volume);
