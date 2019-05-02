1f0f8a5ad64e356ac8b1e5d3b856fd167ab501d8
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-02 11:30:43 +0500

    automatic commit and push

--- temp.c
+++ temp.c
@@ -4,6 +4,6 @@ int main(void){
 	float with_tax;
 	printf("Enter Amount\n");
 	scanf("%f", &amount);
-    printf("With tax added: %1f\n", amount * 1.05);
+    printf("With tax added: %f\n", amount * 1.05);
     return 0;
 }
