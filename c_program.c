13d2c554a1d53e14e4183b95cdf0609766fbfeeb
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-02 11:15:48 +0500

    automatic commit and push

--- c_program.c
+++ c_program.c
@@ -1,8 +1,8 @@
 
 #include <stdio.h>
 int main(void){
-    int radius=10, pie=3.14;
-    float volume = (4.0f/3.0f)//*pie*(radius*radius*radius);
-    printf("Volume: %f\n", volume);
+    //int radius=10, pie=3.14;
+    //float volume = (4.0f/3.0f)*pie*(radius*radius*radius);
+    //printf("Volume: %f\n", volume);
     return 0;
 }
