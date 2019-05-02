400c7ac6868e32f776da36bcf1a60c5dde5bcdb7
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-02 12:24:06 +0500

    automatic commit and push

--- temp.c
+++ temp.c
@@ -4,26 +4,26 @@ int main(void){
 	printf("Enter a number\n");
 	scanf("%d", &dollers);
 	twentys = (dollers - (dollers % 20)) / 20;
-	print("twentys: %d\n", twentys);
+	printf("twentys: %d\n", twentys);
 	balance = dollers - (twentys * 20);
-	print("balance %d", balance);
+	printf("balance %d", balance);
 	ten = (balance - (balance % 10)) / 10;
-	print("ten %d\n", ten);
+	printf("ten %d\n", ten);
 	balance = balance - (balance * 10);
-	print("balance %d\n", balance);
+	printf("balance %d\n", balance);
 	// if ((balance % 5) > 5){
 	five = (balance - (balance % 5)) / 5;
-	print("five %d\n", five);
+	printf("five %d\n", five);
 		// } else {
 	// 	five = 0;
 	// };
 	// printf("%d%d", balance, (balance % 5));
 	balance = balance - (balance * 5);
-	print("balance %d\n", balance);
+	printf("balance %d\n", balance);
 	one = (balance - (balance % 1)) / 1;
-	print("one %d\n", one);
+	printf("one %d\n", one);
 	balance = balance - (balance * 1);
-	print("balance %d\n", balance);
+	printf("balance %d\n", balance);
 	// printf("%d", five);//, , , one
 	// printf("%d", five);
     return 0;
