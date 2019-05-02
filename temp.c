538052865333348c577b9264f95127fe51a413bd
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-02 11:42:29 +0500

    automatic commit and push

--- temp.c
+++ temp.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main(void){
-	int x;
+	int dollers;
 	printf("Enter a number\n");
-	scanf("%d", &x);
-    printf("Polynomial: %d\n", ((((3*x + 2)*x-5)*x-1)*x+7)*x-6);
+	scanf("%d", &dollers);
+	printf("%f", dollers%20);
     return 0;
 }
