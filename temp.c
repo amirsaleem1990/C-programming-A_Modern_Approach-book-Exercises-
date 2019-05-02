5fb846750134680878569268c47810195bee3a91
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-02 12:05:39 +0500

    automatic commit and push

--- temp.c
+++ temp.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 int main(void){
-	int dollers, twentys,balance, ten;
+	int dollers, twentys,balance, ten, five, one;
 	printf("Enter a number\n");
 	scanf("%d", &dollers);
 	twentys = (dollers - (dollers % 20)) / 20;
 	balance = dollers - (twentys * 20);
 	ten = (balance - (balance % 10)) / 10;
 	balance = balance - (balance * 10);
-	printf("%d", ten);
+	five = (balance - (balance % 5)) / 5;
+	balance = balance - (balance * 5);
+	one = (balance - (balance % 1)) / 1;
+	balance = balance - (balance * 1);
+
+
+	printf("%d", twentys, ten, five, one);
     return 0;
 }
