8af7276ab8805d1b795d1b47d05ca28cf70b5335
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-04 19:29:23 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -4,11 +4,11 @@ int main(void){
 	int item_numer, month, day, year;
 	float unit_price;
 	printf("Enter item number: \n");
-	// scanf("%d", &item_numer);
+	scanf("%d", &item_numer);
 	printf("Enter unit price: \n");
-	// scanf("%f", &unit_price);
+	scanf("%f", &unit_price);
 	printf("Enter purchse date(mm/dd/yyyy): \n");
-	// scanf("%d%d%d", &month, &day, &year);
-	printf("%-20s\n", "amir saleem");
+	scanf("%d/%d/%d", &month, &day, &year);
+	printf("%-20s\n", );
 	return 0;
 }
