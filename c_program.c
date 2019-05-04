409e6be66f6718aa00183ba0a321833a9f723e18
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-04 17:20:56 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -8,12 +8,13 @@ int main(void){
     // if (a1 == a2){
     	// printf("Bothe are same\n");
     // }
-	    printf("Enter 3 numbers saperated by comma:        ");
-	    scanf("%d-%d-%d", &b11, &b12, &b13);
-		printf("Enter same previaus 3 numbers     :        ");
-	    scanf("%d -%d -%d", &b21, &b22, &b23);
-    if (b11 == b21 & b12 == b22 & b13 == b23){
-    	printf("All paires are same\n");
-    }
+	 //    printf("Enter 3 numbers saperated by minus-sign:        ");
+	 //    scanf("%d-%d-%d", &b11, &b12, &b13);
+		// printf("Enter same previaus 3 numbers          :        ");
+	 //    scanf("%d -%d -%d", &b21, &b22, &b23);
+  //   if (b11 == b21 & b12 == b22 & b13 == b23){
+  //   	printf("All paires are same\n");
+  //   }
+    
     return 0;
 }
