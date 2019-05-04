58456aeb4ed8e89574429dc2cc8160bb87a5d952
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-04 16:57:04 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -1,9 +1,9 @@
 
-#include <stdio.h>
+# include <stdio.h>
 int main(void){
-    float x=3.252;
-    printf("%c:----%6.1e\n",'a', x);
-    printf("%c:----%-10.6e\n",'b', x);
-    printf("%c:----%8.3f\n", 'c',x);
-    printf("%c:----%-8.0f\n", 'd', x);
-    }
+    int a, b;
+    scanf("%d", &a);
+    scanf(" %d", &b);
+    printf("%d%d", a,b);
+    return 0;
+}
