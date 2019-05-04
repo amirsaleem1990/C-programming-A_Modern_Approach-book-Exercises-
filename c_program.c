189c7c7e68f56a651bd086d95da5b120a58faf0c
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-04 19:42:36 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -10,6 +10,6 @@ int main(void){
 	printf("Enter purchse date(mm/dd/yyyy): \n");
 	scanf("%d/%d/%d", &month, &day, &year);
 	printf("%-30s%-30s%-30s%14s%29s\n", "Item", "Unit", "Purchase\n", "Price", "Date");
-	printf("%-30d$%-30.2f%d/%d/%d", item_number, unit_price, month, day, year);
+	printf("%-30d$%-29.2f%d/%d/%d\n", item_number, unit_price, month, day, year);
 	return 0;
 }
