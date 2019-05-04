fc2eaae72e90a31ee9a89655fbdbbc5ac578bd41
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-04 20:20:33 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -2,9 +2,9 @@
 int main(void){
 	int a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p;
 	scanf("%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d", &a,&b,&c,&d,&e,&f,&g,&h,&i,&j,&k,&l,&m,&n,&o,&p);
-	printf("%d   %d   %d   %d\n", a,b,c,d);
+	printf("\n%d   %d   %d   %d\n", a,b,c,d);
 	printf("%d   %d   %d   %d\n", e,f,g,h);
 	printf("%d  %d  %d  %d\n", i,j,k,l);
-	printf("%d %d %d %d", m,n,o,p);
+	printf("%d %d  %d  %d\n", m,n,o,p);
 	return 0;
 }
