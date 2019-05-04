ad81bf28ef57962a4e8d2073ced09366a2ed5656
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-04 19:59:53 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -2,8 +2,8 @@
 int main(void){
 	int a,b,c;
 	printf("Enter phone number [(xxx) xxx-xxxx]: ");
-	scanf("%d %d %d", &a,&b,&c);
-	printf("You entered %d.%d.%d", a,b,c);
+	scanf("(%d) %d %d", &a,&b,&c);
+	printf("You entered %d.%d.%d\n", a,b,c);
 
 	return 0;
 }
