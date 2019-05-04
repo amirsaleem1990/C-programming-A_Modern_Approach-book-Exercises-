8a3061de879e925ca4736b015a637f7fb196e378
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-04 19:20:15 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -4,7 +4,6 @@ int main(void){
 	int m,d,y;
 	printf("Enter a data(mm/dd/yyyy): ");
 	scanf("%d/%d/%d", &m, &d, &y);
-	// printf("You entered the date %d%d%d", y,m,d);
-	print("%d[0]", m);
+	printf("You entered the date %d0%d%d", y,m,d);
 	return 0;
 }
