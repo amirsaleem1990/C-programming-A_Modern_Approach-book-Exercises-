e0c4a0b90214f86991f9d636fbd707ec641e3764
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-04 19:07:33 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -2,7 +2,7 @@
 # include <stdio.h>
 int main(void){
 	int m,d,y;
-	pintf("Enter a data(mm/dd/yyyy): ");
+	printf("Enter a data(mm/dd/yyyy): ");
 	scanf("%d/%d/%d", &m, &d, &y);
 	printf("You entered the date %d%d%d", m,d,y);
 	return 0;
