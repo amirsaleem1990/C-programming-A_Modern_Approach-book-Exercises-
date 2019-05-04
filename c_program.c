df7bafcc7b9b24f9ab8719086028469c51197412
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-04 20:18:31 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -1,7 +1,7 @@
 # include <stdio.h>
 int main(void){
 	int a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p;
-	scanf("%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d\n", &a,&b,&c,&d,&e,&f,&g,&h,&i,&j,&k,&l,&m,&n,&o,&p);
+	scanf("%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d", &a,&b,&c,&d,&e,&f,&g,&h,&i,&j,&k,&l,&m,&n,&o,&p);
 	printf("%d%d%d%d\n", a,b,c,d);
 	printf("%d%d%d%d\n", e,f,g,h);
 	printf("%d%d%d%d\n", i,j,k,l);
