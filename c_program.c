2168733597766ffbb00d37aa2e9a6cd8da967158
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-04 19:31:55 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -3,12 +3,12 @@
 int main(void){
 	int item_numer, month, day, year;
 	float unit_price;
-	printf("Enter item number: \n");
-	scanf("%d", &item_numer);
-	printf("Enter unit price: \n");
-	scanf("%f", &unit_price);
-	printf("Enter purchse date(mm/dd/yyyy): \n");
-	scanf("%d/%d/%d", &month, &day, &year);
-	printf("%-20s%-20s%-20s\n", "Item", "Unit\nPrice", "Purchase\nDate");
+	// printf("Enter item number: \n");
+	// scanf("%d", &item_numer);
+	// printf("Enter unit price: \n");
+	// scanf("%f", &unit_price);
+	// printf("Enter purchse date(mm/dd/yyyy): \n");
+	// scanf("%d/%d/%d", &month, &day, &year);
+	printf("%-30s%-30s%-30s\n", "Item", "Unit\nPrice", "Purchase\nDate");
 	return 0;
 }
