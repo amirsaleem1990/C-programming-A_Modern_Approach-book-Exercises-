096b7b1047989b40af24fd7bd4fa5559b23eaa79
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-04 17:13:19 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -1,12 +1,19 @@
 # include <stdio.h>
 int main(void){
-    int a1, a2;
-    printf("Enter a1:     ");
-    scanf("%d", &a1);
-    printf("Enter a2:     ");
-    scanf(" %d", &a2);
-    if (a1 == a2){
-    	printf("Bothe are same\n");
+    int a1, a2, b11, b12, b13, b21, b22, b23;
+    // printf("Enter a1:     ");
+    // scanf("%d", &a1);
+    // printf("Enter a2:     ");
+    // scanf(" %d", &a2);
+    // if (a1 == a2){
+    	// printf("Bothe are same\n");
+    // }
+    printf("Enter 3 numbers saperated by comma");
+    scanf("%d-%d-%d", &b11, &b12, &b13);
+	printf("Enter same varius 3 numbers");
+    scanf("%d -%d -%d", &b21, &b22, &b23);    
+    if (b11 == b21){
+    	printf("b11 and b21 are equal");
     }
     return 0;
 }
