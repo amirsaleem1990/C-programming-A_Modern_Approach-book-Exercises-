6e11652f297d582c02d41a426d7d0bce9dd85ac0
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-04 16:52:30 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 int main(void){
     float x=3.252;
-    printf("%6.1e\n", x);
-    printf("%-10.6e\n", x);
-    printf("%8.3f\n", x);
-    printf("%-8.0f\n", x);
+    printf("%c:%6.1e\n",a, x);
+    printf("%c:, %-10.6e\n",b, x);
+    printf("%c:%8.3f\n", c,x);
+    printf("%c:%-8.0f\n", d, x);
     }
