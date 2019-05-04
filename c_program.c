fb4acfe84d1cff38055ae3225029e61cee6b40e9
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-04 19:43:37 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -1,12 +1,12 @@
 /* Adds two fractions */
 # include <stdio.h>
 int main(void){
-	int item_number=583, month, day, year;
-	float unit_price=13.5;
-	// printf("Enter item number: \n");
-	// scanf("%d", &item_number);
-	// printf("Enter unit price: \n");
-	// scanf("%f", &unit_price);
+	int item_number, month, day, year;
+	float unit_price;
+	printf("Enter item number: \n");
+	scanf("%d", &item_number);
+	printf("Enter unit price: \n");
+	scanf("%f", &unit_price);
 	printf("Enter purchse date(mm/dd/yyyy): \n");
 	scanf("%d/%d/%d", &month, &day, &year);
 	printf("\n%-30s%-30s%-30s%14s%29s", "Item", "Unit", "Purchase\n", "Price", "Date");
