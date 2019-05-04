312b1a38f673e1b3dfd97b582b611cf8c80b5271
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-04 17:15:21 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -11,7 +11,8 @@ int main(void){
     // printf("Enter 3 numbers saperated by comma");
     // scanf("%d-%d-%d", &b11, &b12, &b13);
 	printf("Enter same varius 3 numbers");
-    scanf("%d-%d-%d", &b21, &b22, &b23);    
+    scanf("%d-%d-%d", &b21, &b22, &b23);
+    printf("%d%d%d", b21,b22,b23); 
     if (b11 == b21){
     	printf("b11 and b21 are equal");
     }
