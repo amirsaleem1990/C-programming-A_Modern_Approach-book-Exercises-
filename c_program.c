29776c63cb259ea92ff4f1287fe756db48c50c17
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-04 17:08:45 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -1,9 +1,9 @@
 # include <stdio.h>
 int main(void){
     int a1, a2;
-    print("Enter a1");
+    printf("Enter a1:     ");
     scanf("%d", &a1);
-    print("Enter a2");
+    printf("Enter a2:     ");
     scanf(" %d", &a2);
     printf("%d%d\n", a1,a2);
     if (a1 == a2){
