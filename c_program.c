cdaed920e8210499c2f6a7f5dc44add7c96880ec
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-04 16:29:08 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -1,13 +1,9 @@
 
 #include <stdio.h>
 int main(void){
-    printf("%6d, %4d
-", 86, 1040);
-    printf("%12.5e
-", 30.253);
-    printf("%.4f
-", 83.162);
-    printf("%-6.2g
-", .0000009979);
+    printf("%6d, %4d\n", 86, 1040);
+    printf("%12.5e\n", 30.253);
+    printf("%.4f\n", 83.162);
+    printf("%-6.2g\n", .0000009979);
     return 0;
 }
