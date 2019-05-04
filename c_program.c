f4fb9a6866a2ed817ba6a9a14074215bee94c21d
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-04 19:53:16 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -1,8 +1,9 @@
 # include <stdio.h>
 int main(void){
-	int gsi, group, pub, item, check;
-	printf("Enter ISBN: ");
-	scanf("%d-%d-%d-%d-%d",&gsi, &group, &pub, &item, &check);
-	printf("GSI prefix: %d\nGroup identifier: %d\nPublisher code: %d\nItem number: %d\nCheck digit: %d\n", gsi, group, pub, item, check);
+	int a,b,c;
+	pint("Enter phone number [(xxx) xxx-xxxx]: ");
+	scanf("(%d)%d%d", a,b,c);
+	printf("You entered %d.%d.%d")
+
 	return 0;
 }
