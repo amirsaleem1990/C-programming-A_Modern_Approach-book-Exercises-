e99ec21c132a7af63d8a9ae21d4b48798f5a9d50
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-05 21:04:44 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -3,6 +3,6 @@ int main(void){
 	int deg;
 	printf("Enter a two-digit nuber: ");
 	scanf("%d", &deg);
-	printf("The reversal is        :%d%d%d\n", deg%10, (deg/10)%10, (deg/10)/10);
+	printf("The reversal is        : %d%d%d\n", deg%10, (deg/10)%10, (deg/10)/10);
 	return 0;
 }
