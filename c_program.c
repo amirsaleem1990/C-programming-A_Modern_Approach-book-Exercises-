2de6f855a7508af409e996539e64c78d42bc46a1
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-05 23:42:08 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 int main(void){
     int i=1, j=2;
-    //printf("i1: %d\n", j);
+    printf("i1: %d, j1:\n", i,j);
     printf("%d\n", i += j); // 1 += 2 : 3
-    //printf("i2: %d\n", j);
+    printf("i2: %d, j2:\n", i,j);
     printf("%d\n", i--);    // 3
-    //printf("i3: %d\n", j);
+    printf("i3: %d, j3:\n", i,j);
     printf("%d\n", i*j/i);  // 2*2/2
-    //printf("i4: %d\n", j);
+    printf("i4: %d, j4:\n", i,j);
     printf("%d\n", i % ++j);// 2 % 
 }
