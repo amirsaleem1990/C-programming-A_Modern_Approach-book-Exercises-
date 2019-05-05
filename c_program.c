c0ba2a4d20d4ae20e46dcd9f4d9194eb47d4ca64
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-05 23:44:40 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -3,12 +3,12 @@
 int main(void){
     int i=1, j=2;
     printf("i1: %d, j1:%d\n", i,j);
-    printf("%d\n", i += j); // 1 += 2 : 3
+    printf("1: %d\n", i += j); // 1 += 2 : 3
     printf("i2: %d, j2:%d\n", i,j);
-    printf("%d\n", i--);    // 3
+    printf("2: %d\n", i--);    // 3
     printf("i3: %d, j3:%d\n", i,j);
-    printf("%d\n", i*j/i);  // 2*2/2
+    printf("3: %d\n", i*j/i);  // 2*2/2
     printf("i4: %d, j4:%d\n", i,j);
-    printf("%d\n", i % ++j);// 2 % 
+    printf("4: %d\n", i % ++j);// 2 % 
     printf("i5: %d, j5:%d\n", i,j);
 }
