42e990294a12a85445ae3b5bdc5033889c40a49a
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-05 21:27:09 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -1,8 +1,14 @@
 # include <stdio.h>
 int main(void){
-	int a;
-	printf("Enter a number between 0 and 32767:     ");
-	scanf("%d", &a);
-	printf("In octal, your number is          :     0%o\n", a);
+	int a,b,c,d,e,f,g,h,i,j,k, aa, bb, cc, dd, ee, ff;
+	printf("Enter the first 11 digits of a UPS: ");
+	scanf("%d%d%d%d%d%d%d%d%d%d%d", a,b,c,d,e,f,g,h,i,j,k);
+	aa = a+c+e+g+i+k;
+	bb = b+d+f+h+j;
+	cc = aa * 3 + bb;
+	dd = 1- cc;
+	ee = ee % 10;
+	ff = 9-ee
+	printf("Check digit: 5");
 	return 0;
 }
