f349b2a2973ce87fd9385f176f9f355362025a20
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-05 21:46:27 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -1,19 +1,18 @@
 # include <stdio.h>
 int main(void){
 	int i,j,k;
-	float i;
-	j = 3.0
-	i = 5.0; j=3;
+	float i, j;
+	i = 5.0; j=3.0;
 	printf("a:  %d\n", i/j);//, i%j);
     
     i=2; j=3;
-    printf("b:  %d\n", (i+10)%j);
+    // printf("b:  %d\n", (i+10)%j);
     
     i = 7; j=8, k=9;
-    printf("c:  %d\n", (i+10) % k/j);
+    // printf("c:  %d\n", (i+10) % k/j);
     // printf("c:  %d\n", (i+10) % k/j);
     
     i=1; j=2, k=3;
-    printf("d:  %d\n", (i+5) % (j+2) / k);
+    // printf("d:  %d\n", (i+5) % (j+2) / k);
 	return 0;
 }
