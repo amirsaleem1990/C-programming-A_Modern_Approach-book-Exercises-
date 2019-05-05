b958790df11146cb25113fe80b6b60d7ade30be6
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-05 23:52:47 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -1,14 +1,7 @@
 
 #include <stdio.h>
 int main(void){
-    int i=1, j=2;
-    printf("i1: %d, j1:%d\n", i,j);
-    printf("1 : %d\n", i += j); // 1 += 2 : 3
-    printf("i2: %d, j2:%d\n", i,j);
-    printf("2 : %d\n", i--);    // 3
-    printf("i3: %d, j3:%d\n", i,j);
-    printf("3 : %d\n", i*j/i);  // 2*2/2
-    printf("i4: %d, j4:%d\n", i,j);
-    printf("4 : %d\n", i % ++j);// 2 % 
-    printf("i5: %d, j5:%d\n", i,j);
+    float a=5.0, b=2.0; c=3.0; d=6.0; e=4.0;
+    printf(a * b - c * d + e);
+    return 0;
 }
