a67e92869b9e39d39a4119dd8d58eea77fdba51e
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-05 21:42:54 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -9,7 +9,8 @@ int main(void){
     printf("b:  %d\n", (i+10)%j);
     
     i = 7; j=8, k=9;
-    printf("c:  %d\n", (i+10) % k/j);
+    i = 17 % k;
+    printf("c:  %d\n", i/j);
     
     i=1; j=2, k=3;
     printf("d:  %d\n", (i+5) % (j+2) / k);
