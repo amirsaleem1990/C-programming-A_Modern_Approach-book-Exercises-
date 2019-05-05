cab913d7e3d00b73acf06096b5b0ce14bb1ba676
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-05 11:45:11 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -6,7 +6,8 @@ int main(void){
 	printf("%-5d%-5d%-5d%-5d\n", e,f,g,h);
 	printf("%-5d%-5d%-5d%-5d\n", i,j,k,l);
 	printf("%-5d%-5d%-5d%-5d\n", m,n,o,p);
-	printf("Row sums     : %d %d %d %d\n", a+b+c+d, e+f+g+h, i+j+k+l, m+n+o+p);
-	printf("Columns sums : %d %d %d %d\n", a+e+i+m, b+f+j+n, c+g+k+o, d+h+l+p);
+	printf("Row sums       : %d %d %d %d\n", a+b+c+d, e+f+g+h, i+j+k+l, m+n+o+p);
+	printf("Columns sums   : %d %d %d %d\n", a+e+i+m, b+f+j+n, c+g+k+o, d+h+l+p);
+	printf("Diagonal sums  : %d %d", a+f+k+p, d+g+j+m);
 	return 0;
 }
