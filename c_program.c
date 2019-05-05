325366ad61547ef6743d2087d73ca00289de07f9
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-05 11:39:49 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -1,10 +1,11 @@
 # include <stdio.h>
 int main(void){
-	int a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p;
+	int a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,row_sums;
 	scanf("%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d", &a,&b,&c,&d,&e,&f,&g,&h,&i,&j,&k,&l,&m,&n,&o,&p);
 	printf("\n%-5d%-5d%-5d%-5d\n", a,b,c,d);
 	printf("%-5d%-5d%-5d%-5d\n", e,f,g,h);
 	printf("%-5d%-5d%-5d%-5d\n", i,j,k,l);
 	printf("%-5d%-5d%-5d%-5d\n", m,n,o,p);
+	printf("Row sums: %d %d %d %d", a+b+c+d, e+f+g+h, i+j+k+l, m+n+o+p);
 	return 0;
 }
