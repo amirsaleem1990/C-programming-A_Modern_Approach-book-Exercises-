d3f690024a29fe3764b2e21f3be60094fa03af6e
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-05 20:02:28 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -1,10 +1,8 @@
 # include <stdio.h>
 int main(void){
-	int num1, denom1, num2, denom2, result_num, result_denom;
-	printf("Enter two fractions saparated by a plus sign: ");
-	scanf("%d/%d%d/%d", &num1, &denom1, &num2, &denom2);  
-	result_num = num1 * denom2 + num2 * denom1;
-	result_denom = denom1 * denom2;
-	printf("The sum is %d/%d\n", result_num, result_denom);
+	int d;
+	printf("Enter a two-digit nuber: ");
+	scanf("%d", &d);
+	printf(d%10);
 	return 0;
 }
