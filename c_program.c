c7ee1fd5cb9a430b8ed9a708c1614459219209b3
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-05 11:47:53 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -1,13 +1,13 @@
 # include <stdio.h>
 int main(void){
-	int a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p;
-	scanf("%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d%d", &a,&b,&c,&d,&e,&f,&g,&h,&i,&j,&k,&l,&m,&n,&o,&p);
-	printf("\n%-5d%-5d%-5d%-5d\n", a,b,c,d);
-	printf("%-5d%-5d%-5d%-5d\n", e,f,g,h);
-	printf("%-5d%-5d%-5d%-5d\n", i,j,k,l);
-	printf("%-5d%-5d%-5d%-5d\n", m,n,o,p);
-	printf("Row sums       : %d %d %d %d\n", a+b+c+d, e+f+g+h, i+j+k+l, m+n+o+p);
-	printf("Columns sums   : %d %d %d %d\n", a+e+i+m, b+f+j+n, c+g+k+o, d+h+l+p);
-	printf("Diagonal sums  : %d %d", a+f+k+p, d+g+j+m);
+	int num1, denom1, num2, denom2, result_num, result_denom;
+	printf("Enter first fraction: ");
+	scanf("%d/%d", &num1, &denom1); 
+
+	printf("Enter second fraction: ");
+	scanf("%d/%d", &num2, &denom2); 
+	result_num = num1 * denom2 + num2 * denom1;
+	result_denom = denom1 * denom2;
+	printf("The sum is %d/%d\n", result_num, result_denom);
 	return 0;
 }
