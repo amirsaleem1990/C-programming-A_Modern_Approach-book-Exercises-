852eb313ea4c2786d06478bab9c6a961abeca23a
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-05 21:34:47 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -1,14 +1,7 @@
 # include <stdio.h>
 int main(void){
-	int a,b,c,d,e,f,g,h,i,j,k, aa, bb, cc, dd, ee, ff;
-	printf("Enter the first 11 digits of a UPS: ");
-	scanf("%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d", &a,&b,&c,&d,&e,&f,&g,&h,&i,&j,&k);
-	aa = a+c+e+g+i+k;
-	bb = b+d+f+h+j;
-	cc = aa * 3 + bb;
-	dd = 1 - cc;
-	ee = ee % 10;
-	ff = 9 - ee;
-	printf("Check digit: %d\n", ff);
+	int i,j,k;
+	i = 5; j=3;
+	printf("%d %d", i/j, i%j);
 	return 0;
 }
