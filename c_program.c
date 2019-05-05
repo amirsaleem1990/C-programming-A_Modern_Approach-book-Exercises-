277c8effeebdadb4da386326a769da0d27524c3f
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-05 14:14:15 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -2,7 +2,7 @@
 int main(void){
 	int num1, denom1, num2, denom2, result_num, result_denom;
 	printf("Enter two fractions saparated by a plus sign: ");
-	scanf("%d/%d + %d/%d", &num1, &denom1, num2, &denom2);  
+	scanf("%d/%d'+'%d/%d", &num1, &denom1, num2, &denom2);  
 	result_num = num1 * denom2 + num2 * denom1;
 	result_denom = denom1 * denom2;
 	printf("The sum is %d/%d\n", result_num, result_denom);
