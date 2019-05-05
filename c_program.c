08a6fa86bd555ed7d483b4cb3658407fee05fe65
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-05 21:16:57 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -3,6 +3,6 @@ int main(void){
 	int a;
 	printf("Enter a number between 0 and 32767");
 	scanf("%d", &a);
-	printf("In octal, your number is: ");
+	printf("In octal, your number is: %o", a);
 	return 0;
 }
