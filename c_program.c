b602791fc70150baf55250c3cbaf5a58816f35a7
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-05 21:44:25 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -3,13 +3,13 @@ int main(void){
 	int i,j,k;
 	
 	i = 5; j=3;
-	printf("a:  %d %d\n", i/j, i%j);
+	printf("a:  %d %d\n", i/j);//, i%j);
     
     i=2; j=3;
     printf("b:  %d\n", (i+10)%j);
     
     i = 7; j=8, k=9;
-    printf("c:  %d\n", (i+10) % k);
+    printf("c:  %d\n", (i+10) % k/j);
     // printf("c:  %d\n", (i+10) % k/j);
     
     i=1; j=2, k=3;
