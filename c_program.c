740459e7e9d82a030a8cb08b465496ec79023443
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-05 23:27:55 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -1,13 +1,13 @@
 # include <stdio.h>
 int main(void){
-	int a,b,c,d,e,f,g,h,i,j,k,l, aa, bb, cc, dd, ee, ff;
-	printf("Enter the first 12 digits of an EAN: ");
-	scanf("%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d", &a,&b,&c,&d,&e,&f,&g,&h,&i,&j,&k, &l);
+	int a,b,c,d,e,f,g,h,i,j,k, aa, bb, cc, dd, ee, ff;
+	printf("Enter the first 11 digits of a UPS: ");
+	scanf("%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d", &a,&b,&c,&d,&e,&f,&g,&h,&i,&j,&k);
 	aa = a+c+e+g+i+k;
-	bb = b+d+f+h+j+l;
-	cc = bb * 3 + aa;
-	dd = cc - 1;
-	ee = dd % 10;
+	bb = b+d+f+h+j;
+	cc = aa * 3 + bb;
+	dd = 1 - cc;
+	ee = ee % 10;
 	ff = 9 - ee;
 	printf("Check digit: %d\n", ff);
 	return 0;
