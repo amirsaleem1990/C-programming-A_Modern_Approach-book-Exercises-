e6f1c71e55de7a81e820e59ee347815cc701f6e3
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-05 21:17:58 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -1,8 +1,8 @@
 # include <stdio.h>
 int main(void){
 	int a;
-	printf("Enter a number between 0 and 32767\n");
+	printf("Enter a number between 0 and 32767:     ");
 	scanf("%d", &a);
-	printf("In octal, your number is       : 0%o", a);
+	printf("In octal, your number is          :     0%o\n", a);
 	return 0;
 }
