b0537aa14332deaa91e202cbe8c4f75bf99ab91f
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-05 22:02:42 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -1,13 +1,5 @@
 # include <stdio.h>
 int main(void){
-	int i=1,j=99;
-	// scanf("%1d%1d", &i, &j);
-	// printf("%d\n", (-i)/j);
-	// printf("%d\n", -(i/j));
-	for (i; i<100; i++){
-		j--;
-		if (!((-i)/j == -(i/j))){
-			printf("%d\n", i);
-	}}
+	printf("%d", -8/5);
 	return 0;
 }
