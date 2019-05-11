298e82fbc8e4b5b715969efa5937238856ae3adf
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-11 12:22:20 +0500

    updated

---
+++ broker.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(void){
+    int amount, commission;
+    printf("Enter value of trade: \n");
+    scanf("%d", &amount);
+    if (amount < 2500)
+    	commission = 30 + amount * (1.7 / 100);
+    else if (amount < 6250)
+    	commission = 56 + amount * (0.66 / 100);
+    else if (amount < 20000)
+    	commission = 76 + amount * (0.34 / 100);
+    else if (amount < 50000)
+    	commission = 100 + amount * (0.22 / 100);
+    else if (amount < 500000)
+    	commission = 155 + amount * (0.11 / 100);
+    else 
+    	commission = 255 + amount * (0.09 / 100);
+    printf("Commision: $%d\n", commission);
+    return 0;
+}
