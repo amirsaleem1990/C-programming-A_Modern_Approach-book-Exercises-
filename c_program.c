66a63e11774a5a523407dc718e3f3f323aded4ef
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-11 13:23:41 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 int main(void){
 	int m,d,y;
+	char month;
 	printf("Enter date (mm/dd/yy): \n");
 	scanf("%d/%d/%d", &m,&d,&y);
 	switch (m){
-		case 1: m = 'Jan'; break;
-		case 2: m =  "Fab"; break;
-		case 3: m =  "March"; break;
-		case 4: m =  "Apr"; break;
-		case 5: m =  "May"; break;
-		case 6: m =  "Jun"; break;
-		case 7: m =  "Jul"; break;
-		case 8: m =  "Aug"; break;
-		case 9: m =  "Sep"; break;
-		case 10: m =  "Oct"; break;
-		case 11: m =  "Nov"; break;
-		case 12: m =  "Dec"; break;
+		case 1: month = 'Jan'; break;
+		case 2: month =  "Fab"; break;
+		case 3: month =  "March"; break;
+		case 4: month =  "Apr"; break;
+		case 5: month =  "May"; break;
+		case 6: month =  "Jun"; break;
+		case 7: month =  "Jul"; break;
+		case 8: month =  "Aug"; break;
+		case 9: month =  "Sep"; break;
+		case 10: month =  "Oct"; break;
+		case 11: month =  "Nov"; break;
+		case 12: month =  "Dec"; break;
 	};
-	printf("Dated this %dth day of %d,%d", d, m, y);
+	printf("Dated this %dth day of %s,%d", d, month, y);
     return 0;
 }
