6641809bda79b752a38ab55b670f6734020b6200
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-11 14:40:51 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -1,59 +1,11 @@
 #include <stdio.h>
 int main(void){
-	int m,d,y;
-	char month;
-	printf("Enter date (mm/dd/yy): \n");
-	scanf("%d/%d/%d", &m,&d,&y);
-	printf("Dated this %d", d);
-	switch (d){
-		case 1: case 21: case 31:
-			printf("st"); break;
-		case 2: case 22:
-			printf("nd"); break;
-		case 3: case 23:
-			printf("rd"); break;
-		default:
-			printf("th"); break;
+	int n, c;
+	printf("This program prints a table of squares.\nEnter number of entries in table: ");
+	scanf("%d", &n);
+	c = 0;
+	while (c < n){
+		printf("%d         %d\n", c, c * c);
 	}
-	printf(" day of ");
-	switch (m){
-		case 1: 
-			printf("January");
-			break;
-		case 2: 
-			printf( "Fabrury"); 
-			break;
-		case 3: 
-			printf( "March"); 
-			break;
-		case 4: 
-			printf( "April"); 
-			break;
-		case 5: 
-			printf( "May"); 
-			break;
-		case 6: 
-			printf( "June"); 
-			break;
-		case 7: 
-			printf( "July"); 
-			break;
-		case 8: 
-			printf( "Augest"); 
-			break;
-		case 9: 
-			printf( "Septembter"); 
-			break;
-		case 10: 
-			printf( "October"); 
-			break;
-		case 11: 
-			printf( "November"); 
-			break;
-		case 12: 
-			printf( "December"); 
-			break;
-	};
-	printf(", 20%.2d.\n", y);
     return 0;
 }
