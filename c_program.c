4828372f46af23318ad942639221ea678b156ef2
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-11 13:22:25 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -4,19 +4,19 @@ int main(void){
 	printf("Enter date (mm/dd/yy): \n");
 	scanf("%d/%d/%d", &m,&d,&y);
 	switch (m){
-		case 1: m = 'Jan'; break
-		case 2: m =  "Fab"; break
-		case 3: m =  "March"; break
-		case 4: m =  "Apr"; break
-		case 5: m =  "May"; break
-		case 6: m =  "Jun"; break
-		case 7: m =  "Jul"; break
-		case 8: m =  "Aug"; break
-		case 9: m =  "Sep"; break
-		case 10: m =  "Oct"; break
-		case 11: m =  "Nov"; break
-		case 12: m =  "Dec"; break
-	}
-	printf("Dated this %dth day of %d,%d", d, m, y)
+		case 1: m = 'Jan'; break;
+		case 2: m =  "Fab"; break;
+		case 3: m =  "March"; break;
+		case 4: m =  "Apr"; break;
+		case 5: m =  "May"; break;
+		case 6: m =  "Jun"; break;
+		case 7: m =  "Jul"; break;
+		case 8: m =  "Aug"; break;
+		case 9: m =  "Sep"; break;
+		case 10: m =  "Oct"; break;
+		case 11: m =  "Nov"; break;
+		case 12: m =  "Dec"; break;
+	};
+	printf("Dated this %dth day of %d,%d", d, m, y);
     return 0;
 }
