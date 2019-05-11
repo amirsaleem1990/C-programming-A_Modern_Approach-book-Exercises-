5e312aaae50640214427e53163e96bb214691c56
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-11 10:17:55 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -1,8 +1,17 @@
 
 #include <stdio.h>
 int main(void){
-    int i = 6; 
-    i = 2.7;
-    printf("%d", i);
+    i = 6; 
+    j = i += i;
+    printf("a: %d %d\n", i, j);
+    i=5;
+    j = (i -=2) + 1;
+    printf("b: %d %d\n", i, j);
+    i = 7;
+    j = 6 + (i = 2.5);
+    printf("c: %d %d\n", i, j);
+    i = 2; j = 8;
+    j = (i = 6) + (j = 3);
+    printf("d: %d %d\n", i, j);
     return 0;
 }
