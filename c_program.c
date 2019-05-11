869a367cde2efd5e8779eecfadcef356091871fc
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-11 09:24:39 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -1,7 +1,9 @@
 
 #include <stdio.h>
 int main(void){
-    float a=5.0, b=2.0, c=3.0, d=6.0, e=4.0;
-    printf("%f\n", a * b - c * d + e);
+    int i=4;
+    printf("%d\n", ++i);
+    i = 4;
+    printf("%d\n", i++);
     return 0;
 }
