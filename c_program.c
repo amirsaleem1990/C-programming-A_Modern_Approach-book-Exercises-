6d94d449711c4ed2f13d5132df28fe7c902bb40c
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-11 10:16:00 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -1,18 +1,8 @@
 
 #include <stdio.h>
 int main(void){
-    int i,j,k;
-    i=1; 
-    printf("a: %d \n", i++ - 1);
-    printf("a: %d\n", i);
-    i=10; j=5;
-    printf("b: %d \n", i++ - ++j);
-    printf("b: %d %d\n", i, j);
-    i=7; j=8;
-    printf("c: %d \n", i++ - --j);
-    printf("c: %d %d\n", i, j);
-    i=3; j=4; k=5;
-    printf("d: %d\n", i++ - j++ + --k);
-    printf("d: %d %d %d\n", i, j, k);
+    int i = 6; 
+    i = 2.5;
+    printf("%d", i)
     return 0;
-    }
+}
