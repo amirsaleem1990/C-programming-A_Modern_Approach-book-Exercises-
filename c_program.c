536c1d166afad7ecc192bd90022f8981b610dc63
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-18 01:23:49 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -4,23 +4,22 @@ int main(void){
 	int i, j, k;
 	i = 3; j = 4; k = 5;
 	printf('a: %d \n', i < j || ++j < k);
-	printf('a: %d %d %d
+	printf('a: %d %d %d\n', i, j, k);
+
+	i = 7; j = 8; k = 9;
+	printf('b: %d \n', i - 7 && j++ < k);
+	printf('b: %d %d %d
 ', i, j, k);
 
 	i = 7; j = 8; k = 9;
-	printf("b: %d \n", i - 7 && j++ < k);
-	printf("b: %d %d %d
-", i, j, k);
-
-	i = 7; j = 8; k = 9;
-	printf("c: %d \n", (i == j) || (j = k));
-	printf("c: %d %d %d
-", i, j, k);
+	printf('c: %d \n', (i == j) || (j = k));
+	printf('c: %d %d %d
+', i, j, k);
 
 	i = 1; j = 1; k = 1;
-	printf("d: %d \n", ++i || ++j && ++k);
-	printf("d: %d %d %d
-", i, j, k);
+	printf('d: %d \n', ++i || ++j && ++k);
+	printf('d: %d %d %d
+', i, j, k);
 
     return 0;
 }
