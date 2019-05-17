cf2badf7b190f80b8f98e604bffbaeafb6897459
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-18 00:54:50 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -4,11 +4,11 @@ int main(void){
 	int i, j, k;
 	i = 10; j = 5;
 	// printf("a: %d\n", !i < j);
-	printf("%d\n", !i)
+	printf("%d\n", !i);
 
 	i = 2; j = 1;
 	// printf("b: %d\n", !!i + !j);
-	printf("%d,%d\n", !!i, !j)
+	printf("%d,%d\n", !!i, !j);
 	i = 5; j = 0; k = -5;
 	// printf("c: %d\n", i && j || k);
 
