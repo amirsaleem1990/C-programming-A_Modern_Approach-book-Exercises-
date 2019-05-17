d009db3d6d0bb7236efe58e085e35fe8d17d83cc
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-18 01:25:33 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -1,10 +1,10 @@
 
 #include <stdio.h>
 int main(void){
-	int i, j, k;
+	int i; j; k;
 	i = 3; j = 4; k = 5;
-	printf('%d \n', i < j || ++j < k);
-	printf('%d %d %d\n', i, j, k);
+	printf('%d', i < j || ++j < k);
+	printf('%d %d %d', i, j, k);
 
 	i = 7; j = 8; k = 9;
 	printf('b: %d \n', i - 7 && j++ < k);
