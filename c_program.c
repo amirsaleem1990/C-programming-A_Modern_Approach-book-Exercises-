38a3a40618f05b4eb517f1876a39245cdfda96d3
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-18 12:34:25 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -1,6 +1,8 @@
 
 #include <stdio.h>
 int main(void){
-    int i=-17;
-    printf("%d\n", i >= 0 ? i : -i);
+    int i=17;
+    printf("n == 17: %d\n", i >= 0 ? i : -i);
+    i = -17;
+    printf("n == -17: %d\n", i >= 0 ? i : -i);
 }
