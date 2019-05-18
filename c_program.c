be6430994eccc7f7711b0a27e349644736270c94
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-18 12:46:20 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -11,5 +11,5 @@ int main(void){
 		if (age < 13)
 			teenager = 0;
 	printf("%d\n", teenager);
-	printf("%d\n", age >= 13 ? age < 19 : 1 : 0 ? age < 13 : 0);
+	printf("%d\n", age >= 13 ? age < 19 ? 1 : 0 ? age < 13 : 0);
 }
