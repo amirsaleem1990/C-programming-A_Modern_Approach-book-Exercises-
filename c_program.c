59ead481fa8950aed536ab372f05250f1957ef50
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-18 12:24:12 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -1,8 +1,8 @@
 
 #include <stdio.h>
 int main(void){
-	int n=0;
-	if (n >= 1 <= 10)
-		printf("n is between 1 and 10\n");
-    return 0;
+    int n=5;
+    if (n == 1-10);
+        printf("n is between 1 and 10
+");
 }
