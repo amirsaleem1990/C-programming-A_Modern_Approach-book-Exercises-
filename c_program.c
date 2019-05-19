dfebe861ae47dc201a6550cc0a61dec2dc0c1d55
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-19 14:29:36 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -13,7 +13,7 @@ int main(void){
 		printf("The number %d has 4 digit\n", num);
 	else
 		printf("Your number is smaller than 0, please Enter a number in range 0-10000\n");
-		return 0;
-	return 0;
+		// return 0;
 	printf("%d", (num >= 0 & num < 10) ? 1 : (num < 100) ? 2 : (num < 1000) ? 3 : (num < 10000) ? 4 : 50);
+	return 0;
 }
