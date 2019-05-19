1f87c2d4908224996d93ad47fcc4f1d4c181f2f5
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-19 15:25:05 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main(void){
 	float speed;
-	printf("Enter wind speed (in knots)\n");
+	printf("Enter wind speed (in knots)\n\n\n");
 	scanf("%f", &speed);
-	printf("Description: %s", speed < 1 ? "Calm" : speed <= 3 ? "Light air" : speed <= 27 ? "Breeze" : speed <= 47 ? "Gale" : speed <= 63 ? "Strom" : "Hurricane");
+	printf("Speed %2f\nDescription: %s\n", speed, speed < 1 ? "Calm" : speed <= 3 ? "Light air" : speed <= 27 ? "Breeze" : speed <= 47 ? "Gale" : speed <= 63 ? "Strom" : "Hurricane");
 	return 0;
 }
