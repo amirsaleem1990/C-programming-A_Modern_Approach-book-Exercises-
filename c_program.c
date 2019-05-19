15bf73866737c6dd24f2969d87aba7c16531da59
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-19 15:44:37 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 int main(void){
-	float speed;
-	printf("Enter wind speed (in knots):  ");
-	scanf("%f", &speed);
-	printf("\n\n\nSpeed:       %.2f\nDescription: %s\n", speed, speed < 1 ? "Calm" : speed <= 3 ? "Light air" : speed <= 27 ? "Breeze" : speed <= 47 ? "Gale" : speed <= 63 ? "Strom" : "Hurricane");
+	int income;
+	float tax;
+	printf("Enter the amount of taxable income\n");
+	scanf("%d", &income);
+	if (income <= 750)
+		tax = income * 0.01;
+	else if(income <= 2250)
+		tax = 7.5 + (income - 750) * 0.02;
+	else if (income <= 3750)
+		tax = 37.5 + (income - 2250) * 0.03;
+	else if (income <= 5250)
+		tax = 82.5 + (income - 3750) * 0.04;
+	else if (income <= 7000)
+		tax = 142.5 + (income - 5250) * 0.05;
+	else
+		tax = 230 + (income - 7000) * 0.06;
+	printf("\n\nYour tax is %f\n", tax);
 	return 0;
 }
