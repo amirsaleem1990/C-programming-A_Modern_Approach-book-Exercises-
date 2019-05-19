eeb451303d4b36fc871ca529e2365a3c22737005
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-19 15:22:38 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -1,18 +1,8 @@
 #include <stdio.h>
-#include <string.h>
 int main(void){
-	int hour, mint, I2_hours;
-	char am_pm[2];
-	printf("Enter a 24-hour time [eg: 21:11]\n");
-	scanf("%d:%d", &hour, &mint);
-	if (hour > 12) {
-		I2_hours = hour - 12;
-		strcpy(am_pm, "PM");
-	}
-	else {
-		I2_hours = hour;
-		strcpy(am_pm, "AM");
-	}
-	printf("Equivalent 12-hour time: %d:%s %s\n", I2_hours, (mint == 00) ? "00" : "00", am_pm);
+	float speed;
+	printf("Enter wind speed (in knots)\n");
+	scanf("%f", &speed);
+	printf("%s", speed < 1 ? "Calm" : speed < 3 ? "Light air" : speed < 27 ? "Breeze" : speed < 47 ? "Gale" : speed < 63 ? "Strom" : "Hurricane");
 	return 0;
 }
