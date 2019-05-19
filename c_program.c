21e5b448a9c459e5550a48e19da700de31cf5d19
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-19 14:41:16 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -1,23 +1,17 @@
 #include <stdio.h>
 int main(void){
-	int num, n; 
-	printf("Enter a number: \n");
-	scanf("%d", &num);
-	if (num >= 0 & num < 10)
-		printf("The number %d has 1 digit\n", num);
-	else if (num < 100)
-		printf("The number %d has 2 digit\n", num);
-	else if (num < 1000)
-		printf("The number %d has 3 digit\n", num);
-	else if (num < 10000)
-		printf("The number %d has 4 digit\n", num);
-	else
-		printf("Your number is smaller than 0, please Enter a number in range 0-10000\n");
-		// return 0;
-	n = (num >= 0 & num < 10) ? 1 : (num < 100) ? 2 : (num < 1000) ? 3 : (num < 10000) ? 4 : 50;
-	if (n == 50)
-		printf("Your number is smaller than 0, please Enter a number in range 0-10000\n");
-	else
-		printf("The number %d has %d digits\n", num, n);
+	int hour, mint, 12_hours;
+	char am_pm;
+	printf("Enter a 24-hour time [eg: 21:11]\n");
+	scanf("%d:%d", %hour, mint);
+	if hour > 12 {
+		12_hours = hour - 12;
+		am_pm = "PM";
+	}
+	else {
+		12_hours = hour;
+		am_pm = "AM";
+	}
+	printf("Equivalent 12-hour time: %d:%d %s", 12_hours, mint, am_pm);
 	return 0;
 }
