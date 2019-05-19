c3780b7d0b57468a6bfaae9715376daba4c6de08
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-19 14:51:55 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
+#include <strings.h>
 int main(void){
 	int hour, mint, I2_hours;
-	char am_pm;
+	char am_pm[2];
 	printf("Enter a 24-hour time [eg: 21:11]\n");
 	scanf("%d:%d", &hour, &mint);
 	if (hour > 12) {
 		I2_hours = hour - 12;
-		am_pm = 'PM';
+		strcpy(am_pm, "PM");
 	}
 	else {
 		I2_hours = hour;
-		am_pm = "AM";
+		strcpy(am_pm, "AM");
 	}
 	printf("%c", am_pm);
 	// printf("Equivalent 12-hour time: %d:%d %c\n", I2_hours, mint, am_pm);
