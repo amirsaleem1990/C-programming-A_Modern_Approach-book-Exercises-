0b2440139d335c2704462761adde8eb68b96a6aa
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-19 14:25:03 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -4,13 +4,13 @@ int main(void){
 	printf("Enter a number: \n");
 	scanf("%d", &num);
 	if (num >= 0 & num < 10)
-		printf("The number %d has 1 digit", num);
+		printf("The number %d has 1 digit\n", num);
 	else if (num < 100)
-		printf("The number %d has 2 digit", num);
+		printf("The number %d has 2 digit\n", num);
 	else if (num < 1000)
-		printf("The number %d has 3 digit", num);
+		printf("The number %d has 3 digit\n", num);
 	else if (num < 10000)
-		printf("The number %d has 4 digit", num);
+		printf("The number %d has 4 digit\n", num);
 	else
 		printf("Your number is smaller than 0, please Enter a number in range 0-10000\n");
 		return 0;
