c934846f08ef03824fea35d7b27f4dacdb9c1af4
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-19 14:23:49 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -1,28 +1,17 @@
-
 #include <stdio.h>
 int main(void){
-	int score;
-	scanf("%d", &score);
-	if (score >= 90)
-		printf("A");
-	else if (score >= 80)
-		printf("B");
-	else if (score >= 70)
-		printf("C");
-	else if (score >= 60)
-		printf("D");
+	int num; 
+	printf("Enter a number: \n", &num);
+	if (num >= 0 & num < 10)
+		printf("The number %d has 1 digit", num);
+	else if (num < 100)
+		printf("The number %d has 2 digit", num);
+	else if (num < 1000)
+		printf("The number %d has 3 digit", num);
+	else if (num < 10000)
+		printf("The number %d has 4 digit", num);
 	else
-		printf("F");
-
-
-	if (score < 60)
-		printf("F");
-	else if (score < 70)
-		printf("D");
-	else if (score < 80)
-		printf("C");
-	else if (score < 90)
-		printf("B");
-	else
-		printf("A");
+		printf("Your number is smaller than 0, please Enter a number in range 0-10000\n");
+		return 0;
+	return 0;
 }
