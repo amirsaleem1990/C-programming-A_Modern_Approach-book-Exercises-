ca40215a35cd3922e1f7f1cabe1b04c0effa248d
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-19 17:15:17 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -1,21 +1,17 @@
+/* computes a Universal Product check digit */
 #include <stdio.h>
 int main(void){
-	int income;
-	float tax;
-	printf("Enter the amount of taxable income\n");
-	scanf("%d", &income);
-	if (income <= 750)
-		tax = income * 0.01;
-	else if(income <= 2250)
-		tax = 7.5 + (income - 750) * 0.02;
-	else if (income <= 3750)
-		tax = 37.5 + (income - 2250) * 0.03;
-	else if (income <= 5250)
-		tax = 82.5 + (income - 3750) * 0.04;
-	else if (income <= 7000)
-		tax = 142.5 + (income - 5250) * 0.05;
-	else
-		tax = 230 + (income - 7000) * 0.06;
-	printf("\n\nYour tax is %.2f\n", tax);
+	int d, i1, i2, i3, i4, i5, j1, j2, j3, j4, j5,
+	first_sum, second_sum, total;
+	printf("Enter the first (single) digit: \n");
+	scanf("%1d", &d);
+	printf("Enter first group of five digits: \n");
+	scanf("%1d%1d%1d%1d%1d", &i1, &i2, &i3, &i4, &i5);
+	printf("Enter second group of five digits: \n");
+	scanf("%1d%1d%1d%1d%1d", &j1, &j2, &j3, &j4, &j5);
+	first_sum = d + i2 + i4 + j1 + j3 + j5;
+	second_sum = i1 + i3 + i5 + j2 + j4;
+	total = 3 * first_sum + second_sum;
+	printf("Check digit: %d\n", 9- ((total -1) % 10));
 	return 0;
 }
