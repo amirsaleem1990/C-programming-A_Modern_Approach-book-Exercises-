a3250d06b51207e66248cab136a063271bf5cd91
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-19 14:24:26 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 int main(void){
 	int num; 
-	printf("Enter a number: \n", &num);
+	printf("Enter a number: \n");
+	scanf("%d", &num);
 	if (num >= 0 & num < 10)
 		printf("The number %d has 1 digit", num);
 	else if (num < 100)
