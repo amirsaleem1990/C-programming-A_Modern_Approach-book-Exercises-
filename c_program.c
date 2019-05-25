f41ea94efd77927f6c738e0723848ce2321345e6
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-25 22:19:18 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -5,17 +5,13 @@ int main(void){
 	print("Enter four integers: ");
 	scanf("%d%d%d%d", &int1, &int2, &int3, &int4);
 	for (i; i<4; i++){
-		if ((int1 > int2) && (int1 > int3) && )
+		if ((int1 > int2) && (int1 > int3) && (int1 > int4))
 			largest = int1
-			if (int1 > int3)
-				largest = int1
-				if (int1 > int4)
-					largest = int1
-				else
-					largest = int4
-			else
-				largest = int3
-		else
+		else if((int2 > int1) && (int2 > int3) && (int2 > int4))
 			largest = int2
-	}
+		else if((int3 > int1) && (int3 > int2) && (int3 > int4))
+			largest = int3
+		else
+			largest = int4
+	}	
 }
