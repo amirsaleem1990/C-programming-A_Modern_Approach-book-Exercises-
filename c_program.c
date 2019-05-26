3c03aac9530084b9777a395dfae37f2a4d547e16
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-26 14:29:31 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -60,23 +60,18 @@ int main(void){
 	m_a = arrive % 60;	
 	
 	
-	// if (h_d > 12){
-	// 	h_d = h_d % 12;
-	// 	am_pm_d[0]='P';
-	// 	am_pm_d[1] = 'M';
-	// }else {
-	// 	am_pm_d[0]='A';
-	// 	am_pm_d[1] = 'M';
-	// }
+	if (h_d > 12){
+		h_d = h_d % 12;
+		strcpy(am_pm_d, "PM");
+	}else {
+		strcpy(am_pm_d, "AM");
+	}
 	
-	// if (h_a > 12){
-	// 	h_a = h_a % 12;
-	//     am_pm_a[0]='P';
-	// 	am_pm_a[1] = 'M';
-	// }else{ 
-	// 	am_pm_a[0]='A';
-	// 	am_pm_a[1] = 'M';
-	// }
-	strcpy(am_pm_d, "De");
+	if (h_a > 12){
+		h_a = h_a % 12;
+		strcpy(am_pm_a, "PM");
+	}else{ 
+		strcpy(am_pm_a, "AM");
+	}
 	printf("Closest departure time is: %d:%d %s, arriving at %d:%d %s\n",h_d, m_d, am_pm_d, h_a, m_a, am_pm_a);
 }
