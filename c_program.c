d6c453d2a4d5a876591709d2c5c11f3a81bbff75
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-26 23:13:34 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -5,6 +5,23 @@ int main(void){
 	scanf("%2d", &number);
 	first_num = number / 10;
 	second_num = number % 10;
+	if (!(number > 9 && number < 20)){
+	switch (first_num){
+		case 9: printf("Ninty"); break;
+		case 8: printf("Eighty"); break;
+		case 7: printf("Seventy"); break;
+		case 6: printf("Sixty"); break;
+		case 5: printf("Fifty"); break;
+		case 4: printf("Forty"); break;
+		case 3: printf("Thirty"); break;
+		case 2: printf("Twenty"); break;
+		}
+	} else{
+		switch
+	}
+
+
+
 	printf("You entered the number %d\n%d\n", first_num, second_num);
 	return 0;
 }
