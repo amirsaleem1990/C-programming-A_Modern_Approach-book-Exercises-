634da95a532040ba08f8e2bfbca105d3bff8b0dc
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-27 00:25:07 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -1,52 +1,13 @@
+/* Calculates the number of digits in an integer */
 #include <stdio.h>
 int main(void){
-	int number, first_num, second_num;
-	printf("Enter a two-digit number: ");
-	scanf("%d", &number);
-	number = 105;
-	first_num = number / 10;
-	second_num = number % 10;
-	if ((number < 10) || (number > 99)){
-		printf("Please restart, and enter two-digit number\n");
-		return 0;
-	} else if (number > 19){
-		printf("You entered the number ");
-		switch (first_num){
-			case 9: printf("Ninty "); break;
-			case 8: printf("Eighty "); break;
-			case 7: printf("Seventy "); break;
-			case 6: printf("Sixty "); break;
-			case 5: printf("Fifty "); break;
-			case 4: printf("Forty "); break;
-			case 3: printf("Thirty "); break;
-			case 2: printf("Twenty "); break;
-			}
-		switch (second_num){
-			case 1: printf("one\n"); break;
-			case 2: printf("two\n"); break;
-			case 3: printf("three\n"); break;
-			case 4: printf("four\n"); break;
-			case 5: printf("five\n"); break;
-			case 6: printf("six\n"); break;
-			case 7: printf("seven\n"); break;
-			case 8: printf("eight\n"); break;
-			case 9: printf("nine\n"); break;
-		}
-	} else{
-		printf("You entered the number ");
-		switch(number){
-			case 10: printf("Ten\n"); break;
-			case 11: printf("Eleven\n"); break;
-			case 12: printf("Twelve\n"); break;
-			case 13: printf("Thirteen\n"); break;
-			case 14: printf("Fourteen\n"); break;
-			case 15: printf("Fifteen\n"); break;
-			case 16: printf("Sixteen\n"); break;
-			case 17: printf("Seventeen\n"); break;
-			case 18: printf("Eighteen\n"); break;
-			case 19: printf("Ninteen\n"); break;
-		}
-	}
-
+	int digits = 0, n;
+	printf("Enter a nonnegative integer: ");
+	scanf("%d", &n);
+	do{
+		n /= 10;
+		digits ++;
+	} while (n > 0);
+	printf("The number has %d digit(s).\n", digits);
 	return 0;
 }
