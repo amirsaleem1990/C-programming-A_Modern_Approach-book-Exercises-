3ea9b67a259294ebdfdb0b1b397e0ab6276b87e5
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-26 20:02:53 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -3,24 +3,24 @@ int main(void){
 	int grade;
 	printf("Enter numerical grade: ");
 	scanf("%d", &grade);
-	printf("Letter grade: ")
+	printf("Letter grade: ");
 	switch(grade){
 		case 90: case 91: case 92: case 93: case 94: case 95: case 96:
 		case 97: case 98: case 99: case 100: 
-		printf("A"); break;
+		printf("A\n"); break;
 
 		case 80: case 81: case 82: case 83: case 84: 
 		case 85: case 86: case 87: case 88: case 89:
-		printf("B"); break;  
+		printf("B\n"); break;  
 
 		case 70 : case 71 : case 72 : case 73 : case 74 : case 75 : case 76 :
 		case 77 : case 78 : case 79:
-		printf("C"); break;
+		printf("C\n"); break;
 
 
 		case 60 : case 61 : case 62 : case 63 : case 64 : case 65 : case 66 :
 		case 67 : case 68 : case 69 :
-		printf("D"); break;
+		printf("D\n"); break;
 
 
 		case 0 : case 1 : case 2 : case 3 : case 4 : case 5 : case 6 : case 7 :
