115045a97902a3c516fa3b8b83b6a8b797885947
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-26 19:52:37 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -1,33 +1,10 @@
 #include <stdio.h>
 int main(void){
-	int month1, month2, day1, day2, year1, year2, earlear;
-	printf("Enter first date (mm/dd/yy): ");
-	scanf("%d/%d/%d", &month1, &day1, &year1);
-	printf("Enter second date (mm/dd/yy): ");
-	scanf("%d/%d/%d", &month2, &day2, &year2);
-	if (year2 > year1){
-		earlear = 1;
-	}else if(year2 < year1){
-		earlear = 2;
-	}else if (year2 == year1){
-		if (month2 > month1){
-			earlear = 1;
-		}else if(month2 < month1){
-			earlear = 2;
-		}else if(month2 == month1){
-			if (day2 < day1){
-				earlear = 2;
-			} else if(day2 > day1){
-				earlear = 1;
-			} else if (day2 == day1){
-				earlear = 0;
-			}
-		}
+	int grade;
+	printf("Enter numerical grade: ");
+	scanf("%d", &grade);
+	switch(grade){
+		
 	}
-	if (earlear == 1){
-		printf("%d/%d/%d is earlear than %d/%d/%d", month1, day1, year1, month2, day2, year2);
-	}else if(earlear ==2) 
-		printf("%d/%d/%d is earlear than %d/%d/%d", month2, day2, year2, month1, day1, year1);
-	else 
-		printf("Both dates are same");
+	return 0;
 }
