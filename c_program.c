11abf5f726e9649ebbd508bb9376eade386e76f2
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-26 11:48:01 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(void){
-	int int1, int2, int3, int4, largest, i=0;
+	int int1, int2, int3, int4, largest, smallest, i=0;
 
 	printf("Enter four integers: ");
 	scanf("%d%d%d%d", &int1, &int2, &int3, &int4);
@@ -12,5 +12,19 @@ int main(void){
 		largest = int3;
 	else
 		largest = int4;
-	printf("%d\n", largest);	
+
+	if ((int1 < int2) && (int1 < int3) && (int1 < int4))
+		smallest = int1;
+	else if((int2 < int1) && (int2 < int3) && (int2 < int4))
+		smallest = int2;
+	else if((int3 < int1) && (int3 < int2) && (int3 < int4))
+		smallest = int3;
+	else
+		smallest = int4;
+
+
+	printf("Largest: %d\nSmallest: %d\n", largest, smallest);
+
+
+
 }
