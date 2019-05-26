1a07d1fd7722fff7e5fdd7ed5bf1fb0ca7449f45
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-26 12:34:06 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -1,26 +1,36 @@
 #include <stdio.h>
 int main(void){
-	int hours, mints, time;
+	int hours, mints, time, departure;
 
-	printf("Enter a 24-hour time: ");
-	scanf("%d:%d", &hours, &mints);
+	// printf("Enter a 24-hour time: ");
+	// scanf("%d:%d", &hours, &mints);
 
-	time = hours * 60 + mints;
+	// time = hours * 60 + mints;
 
-	int d1=8*60, d2=9*60+43, d3=11*60+19, d4=12*60+47;
-	int d5=(2+12)*60, d6=(3+12)*60+45, d7=(7+12)*60, d8=(9+12)*60+45;
+	// int d1=8*60, d2=9*60+43, d3=11*60+19, d4=12*60+47;
+	// int d5=(2+12)*60, d6=(3+12)*60+45, d7=(7+12)*60, d8=(9+12)*60+45;
 	
-	int a1=10*60+16, a2=11*60+52, a3=(1+12)*60+31, a4=(3+12)*60;
-	int a5=(4+12)*60+8, a6=(5+12)*60+55, a7=(9+12)*60+20, a8=(11+12)*60+58;
+	// int a1=10*60+16, a2=11*60+52, a3=(1+12)*60+31, a4=(3+12)*60;
+	// int a5=(4+12)*60+8, a6=(5+12)*60+55, a7=(9+12)*60+20, a8=(11+12)*60+58;
 
-	printf("%d:%d\n", d1, a1);
-	printf("%d:%d\n", d2, a2);
-	printf("%d:%d\n", d3, a3);
-	printf("%d:%d\n", d4, a4);
-	printf("%d:%d\n", d5, a5);
-	printf("%d:%d\n", d6, a6);
-	printf("%d:%d\n", d7, a7);
-	printf("%d:%d\n", d8, a8);
-	
 
+	// if (time < d1)
+	// 	departure = d1
+	// else if(time > d1 && time < d3)
+	// 	departure = d2
+	// else if (time > d2 && time < d4)
+	// 	departure = d3
+	// else if (time > d3 && time < d5)
+	// 	departure = d4
+	// else if (time > d4 && time < d6)
+	// 	departure = d5
+	// else if (time > d5 && time < d7)
+	// 	departure = d6
+	// else if (time > d6 && time < d8)
+	// 	departure = d7
+	// else
+	// 	departure = d8
+	// if (departure % 12 > 0)
+	// printf("Closest departure time is: %d:%d")
+	printf("%d\n", 840&12);
 }
