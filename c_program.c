5e9051a9b10cf178bef29930c0e953c09ea645eb
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-26 12:25:40 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -13,14 +13,14 @@ int main(void){
 	int a1=10*60+16, a2=11*60+52, a3=(1+12)*60+31, a4=(3+12)*60;
 	int a5=(4+12)*60+8, a6=(5+12)*60+55, a7=(9+12)*60+20, a8=(11+12)*60+58;
 
-	printf("%d:%d\n", d1, a1);
-	printf("%d:%d\n", d2, a2);
-	printf("%d:%d\n", d3, a3);
-	printf("%d:%d\n", d4, a4);
-	printf("%d:%d\n", d5, a5);
-	printf("%d:%d\n", d6, a6);
-	printf("%d:%d\n", d7, a7);
-	printf("%d:%d\n", d8, a8);
+	printf("%d:%d\n", d1/12, a1/12);
+	printf("%d:%d\n", d2/12, a2/12);
+	printf("%d:%d\n", d3/12, a3/12);
+	printf("%d:%d\n", d4/12, a4/12);
+	printf("%d:%d\n", d5/12, a5/12);
+	printf("%d:%d\n", d6/12, a6/12);
+	printf("%d:%d\n", d7/12, a7/12);
+	printf("%d:%d\n", d8/12, a8/12);
 	
 
 }
