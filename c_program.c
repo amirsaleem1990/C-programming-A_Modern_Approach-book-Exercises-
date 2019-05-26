a24b69d554b983925a5b6d9ac3852611249257e9
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-26 20:11:17 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -4,7 +4,7 @@ int main(void){
 	printf("Enter a two-digit number: ");
 	scanf("%2d", &number);
 	first_num = number / 10;
-	second_num = number % 10
+	second_num = number % 10;
 	printf("You entered the number %d\n%d\n", first_num, second_num);
 	return 0;
 }
