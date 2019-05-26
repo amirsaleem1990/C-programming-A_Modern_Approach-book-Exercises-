25ded4900efc7782466f945e37a83594c0d4536c
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-26 23:21:16 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -5,23 +5,43 @@ int main(void){
 	scanf("%2d", &number);
 	first_num = number / 10;
 	second_num = number % 10;
-	if (!(number > 9 && number < 20)){
-	switch (first_num){
-		case 9: printf("Ninty"); break;
-		case 8: printf("Eighty"); break;
-		case 7: printf("Seventy"); break;
-		case 6: printf("Sixty"); break;
-		case 5: printf("Fifty"); break;
-		case 4: printf("Forty"); break;
-		case 3: printf("Thirty"); break;
-		case 2: printf("Twenty"); break;
+	printf("You entered the number ");
+	if (number > 19){
+		switch (first_num){
+			case 9: printf("Ninty "); break;
+			case 8: printf("Eighty "); break;
+			case 7: printf("Seventy "); break;
+			case 6: printf("Sixty "); break;
+			case 5: printf("Fifty "); break;
+			case 4: printf("Forty "); break;
+			case 3: printf("Thirty "); break;
+			case 2: printf("Twenty "); break;
+			}
+		switch (second_num){
+			case 1: printf("one\n"); break;
+			case 2: printf("two\n"); break;
+			case 3: printf("three\n"); break;
+			case 4: printf("four\n"); break;
+			case 5: printf("five\n"); break;
+			case 6: printf("six\n"); break;
+			case 7: printf("seven\n"); break;
+			case 8: printf("eight\n"); break;
+			case 9: printf("nine\n"); break;
 		}
 	} else{
-		switch
+		switch(number){
+			case 10: printf("Ten\n"); break;
+			case 11: printf("Eleven\n"); break;
+			case 12: printf("Twelve\n"); break;
+			case 13: printf("Thirteen\n"); break;
+			case 14: printf("Fourteen\n"); break;
+			case 15: printf("Fifteen\n"); break;
+			case 16: printf("Sixteen\n"); break;
+			case 17: printf("Seventeen\n"); break;
+			case 18: printf("Eighteen\n"); break;
+			case 19: printf("Ninteen\n"); break;
+		}
 	}
 
-
-
-	printf("You entered the number %d\n%d\n", first_num, second_num);
 	return 0;
 }
