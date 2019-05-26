ec5542dff20645c3242ff046d6a4c09a14b8306a
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-26 15:00:17 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -71,7 +71,7 @@ int main(void){
 		am_pm_d = 1; // here 1 means "PM"
 	} else am_pm_d = 0; // here 0 means "AM"
 
-	if (h1 >= 12)
+	if (h_a >= 12)
 		am_pm_a = 1; // here 1 means "PM"
 	else am_pm_a = 0; // here 0 means "AM"
 	printf("Closest departure time is: %d:%d %cM, arriving at %d:%d %cM\n",h_d, m_d, am_pm_d == 1 ? 'P' : 'A', h_a, m_a, am_pm_a == 1 ? 'P' : 'A');
