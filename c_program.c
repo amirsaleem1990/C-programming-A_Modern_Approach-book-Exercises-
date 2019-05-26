e1f8179bdc1283e97759b9cb0f8888eb9d889b58
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-26 19:02:48 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -3,7 +3,7 @@ int main(void){
 	int month1, month2, day1, day2, year1, year2, earlear;
 	printf("Enter first date (mm/dd/yy): ");
 	scanf("%d/%d/%d", &month1, &day1, &year1);
-	printf("Enter second date (mm/dd/yy): ")
+	printf("Enter second date (mm/dd/yy): ");
 	scanf("%d/%d/%d", &month2, &day2, &year2);
 	if (year2 > year1){
 		earlear = 1;
@@ -25,9 +25,9 @@ int main(void){
 		}
 	}
 	if (earlear == 1){
-		printf("%d/%d/%d is earlear than %d/%d/%d", month1, day1, year1);
+		printf("%d/%d/%d is earlear than %d/%d/%d", month1, day1, year1, month2, day2, year2);
 	}else if(earlear ==2) 
-		printf("%d/%d/%d is earlear than %d/%d/%d", month2, day2, year2);
+		printf("%d/%d/%d is earlear than %d/%d/%d", month2, day2, year2, month1, day1, year1);
 	else 
 		printf("Both dates are same");
 }
