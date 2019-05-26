787e2fb22e34218a86176240730e4a6218db34a9
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-26 12:54:32 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -30,7 +30,8 @@ int main(void){
 		departure = d7;
 	else
 		departure = d8;
-	h = departure / 60;
-	m = (departure & 60) * 60; 
-	printf("Closest departure time is: %d:%d\n",h, m);
+	// h = departure / 60;
+	// m = (departure & 60) * 60; 
+	// printf("Closest departure time is: %d:%d\n",h, m);
+	printf("%d", 481 % 60);
 }
