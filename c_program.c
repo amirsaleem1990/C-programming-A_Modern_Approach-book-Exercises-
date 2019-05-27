a1b3d3a011794ca81acf09666226eead31546130
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-27 10:45:17 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -3,25 +3,25 @@ int main(void){
 	int command, balance, amount;
 	printf("*** ACME checkbook-balancing program ***\n");
 	printf("Commands: 0=clear, 1=credit, 2=debit, 3=balance, 4=exit");
-	for(;;) 
+	for(;;){ 
 		printf("Enter command: \n");
 		scanf("%1d", &command);
-		// if (command == 0){
-		// 	return 0;
-		// }
-		// switch (command){
-		// 	case 0: balance = 0; break;
-		// 	case 1: prinft("Enter amount of credit: \n");
-		// 			scanf("%d", &amount);
-		// 			balance += amount; break;
-		// 	case 2: printf("Enter amount of debit: \n");
-		// 			scanf("%d", &amount);
-		// 			balance -= amount; break;
-		// 	case 3: printf("Current balance: $%d\n", balance);
-		// 			break;
-		// 	case 4: return 0;
-		// }
-
+		if (command == 0){
+			return 0;
+		}
+		switch (command){
+			case 0: balance = 0; break;
+			case 1: prinft("Enter amount of credit: \n");
+					scanf("%d", &amount);
+					balance += amount; break;
+			case 2: printf("Enter amount of debit: \n");
+					scanf("%d", &amount);
+					balance -= amount; break;
+			case 3: printf("Current balance: $%d\n", balance);
+					break;
+			case 4: return 0;
+		}
+}
 
 
 
