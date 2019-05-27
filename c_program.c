5fe546e1ee4470e8c7a64b61a50b802cdb4847e7
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-27 10:44:40 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -6,21 +6,21 @@ int main(void){
 	for(;;) 
 		printf("Enter command: \n");
 		scanf("%1d", &command);
-		if (command == 0){
-			return 0;
-		}
-		switch (command){
-			case 0: balance = 0; break;
-			case 1: prinft("Enter amount of credit: \n");
-					scanf("%d", &amount);
-					balance += amount; break;
-			case 2: printf("Enter amount of debit: \n");
-					scanf("%d", &amount);
-					balance -= amount; break;
-			case 3: printf("Current balance: $%d\n", balance);
-					break;
-			case 4: return 0;
-		}
+		// if (command == 0){
+		// 	return 0;
+		// }
+		// switch (command){
+		// 	case 0: balance = 0; break;
+		// 	case 1: prinft("Enter amount of credit: \n");
+		// 			scanf("%d", &amount);
+		// 			balance += amount; break;
+		// 	case 2: printf("Enter amount of debit: \n");
+		// 			scanf("%d", &amount);
+		// 			balance -= amount; break;
+		// 	case 3: printf("Current balance: $%d\n", balance);
+		// 			break;
+		// 	case 4: return 0;
+		// }
 
 
 
