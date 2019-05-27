32c38e5c7864ba68fe1e3690cecb7ff63c951271
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-27 14:59:48 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -1,6 +1,10 @@
 
-i = 1;
-while (i <= 128){
-    printf("%d ", i);
-    i *= 2;
+#include <stdio.h>
+int main(void){
+    i = 1;
+    while (i <= 128){
+        printf("%d ", i);
+        i *= 2;
+    }
+    return 0;
 }
