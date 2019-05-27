0201d42415e369c32deb3bcda95386f7109caf47
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-27 10:56:29 +0500

    updated

--- checking.c
+++ checking.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 int main(void){
 	int command; 
-	float balance=0.0, amount;
+	float balance=0.0f, amount;
 	printf("*** ACME checkbook-balancing program ***\n");
 	printf("Commands: 0=clear, 1=credit, 2=debit, 3=balance, 4=exit\n\n");
 	for(;;){ 
@@ -13,14 +13,18 @@ int main(void){
 		}
 		switch (command){
 			case 0: balance = 0; break;
+
 			case 1: printf("Enter amount of credit: ");
 					scanf("%f", &amount);
 					balance += amount; break;
+
 			case 2: printf("Enter amount of debit: ");
 					scanf("%f", &amount);
 					balance -= amount; break;
+
 			case 3: printf("Current balance: $%.2f", balance);
 					break;
+					
 			case 4: return 0;
 		}
 }
