231b948e1077b08c9813280e8e548f21105b55c4
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-05-27 10:42:07 +0500

    updated

--- c_program.c
+++ c_program.c
@@ -1,13 +1,29 @@
-/* Calculates the number of digits in an integer */
 #include <stdio.h>
 int main(void){
-	int digits = 0, n;
-	printf("Enter a nonnegative integer: ");
-	scanf("%d", &n);
-	do{
-		n /= 10;
-		digits ++;
-	} while (n > 0);
-	printf("The number has %d digit(s).\n", digits);
+	int command, balance, amount;
+	printf("*** ACME checkbook-balancing program ***\n");
+	printf("Commands: 0=clear, 1=credit, 2=debit, 3=balance, 4=exit");
+	for(;;;); 
+		printf("Enter command: \n")
+		scanf("%1d", command);
+		if (command == 0){
+			return 0;
+		}
+		switch (command){
+			case 0: balance = 0; continue;
+			case 1: prinft("Enter amount of credit: \n");
+					scanf("%d", &amount);
+					balance += amount; continue;
+			case 2: printf("Enter amount of debit: \n");
+					scanf("%d", &amount);
+					balance -= amount; continue;
+			case 3: printf("Current balance: $%d\n", balance);
+					continue;
+			case 4: return 0;
+		}
+
+
+
+
 	return 0;
 }
