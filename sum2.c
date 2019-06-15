15b2a05321319ff395f08103490b340b952f9b3c
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-06-15 18:24:18 +0500

    updated

---
+++ sum2.c
@@ -0,0 +1,15 @@
+/* sums a series of number(using long variables) */
+#include <stdio.h>
+int main(void){
+	long n, sum = 0;
+	printf("This program sums a series of integers.\n");
+	printf("Enter intergers (0 to terminate): ");
+	scanf("%ld", &n);
+	while (n != 0){
+		sum += n;
+		scanf("%ld", &n);
+	}
+	printf("The sum is: %ld\n", sum);
+
+	return 0;
+}
